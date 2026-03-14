2b6f571a151712dedf1d61e16f9fa67b2f8165a8
Isabella Hogan <35818862+Isseymay@users.noreply.github.com>
2026-03-15 10:42:14 +1100

    fixing merge conflicts (i hope)

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 import Navbar from "./Components/navbar";
-import { allSearchResults } from "./Components/search";
 import Link from 'next/link';
 
 export default function Home() {
-  const search = "Love me";
   return (
     <div className="page">
       <main>
@@ -12,7 +10,6 @@ export default function Home() {
           <Link href="/start">Start a party</Link>
           <a href="/join">Join a party</a>
         </div>
-        {allSearchResults("Love me")}
       </main>
     </div>
   );
